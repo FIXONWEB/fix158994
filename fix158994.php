36beddd59f141e732b05f41f89821d3ea11daeae
fixonweb <contato@fixonweb.com.br>
2020-05-27 12:04:48 -0300

    0.1.2

--- fix158994.php
+++ fix158994.php
@@ -7,7 +7,7 @@
  * Author URI:      https://github.com/fixonweb
  * Text Domain:     fix158994
  * Domain Path:     /languages
- * Version:         0.1.1
+ * Version:         0.1.2
  *
  * @package         Fix158994
  */
